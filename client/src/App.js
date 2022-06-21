00464ea3ff51173f43e8fd2ca8a5622dd5175401
aiccuracy <soojin.emily@gmail.com>
2022-06-22 01:44:15 +0900

    feat: add router

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Login from "./components/Login.js";
 import MyCalendar from "./components/Calendar.js";
 import IDSearch from "./components/IDSearch.js";
 import PWSearch from "./components/PWSearch.js";
+import SignUp from "./components/SignUp.js";
+import Profile from "./components/Profile.js";
+import DailyExpenditure from "./components/DailyExpenditure.js";
 
 function App() {
     return (
@@ -12,6 +15,7 @@ function App() {
                 <NavBarButton>HOME</NavBarButton>
                 <NavBarButton>CALENDAR</NavBarButton>
                 <NavBarButton>STATS</NavBarButton>
+                <NavBarButton>PROFILE</NavBarButton>
             </NavBar>
 
             <Routes>
@@ -19,6 +23,9 @@ function App() {
                 <Route path="/calendar" element={<MyCalendar />} />
                 <Route path="/idsearch" element={<IDSearch />}></Route>
                 <Route path="/pwsearch" element={<PWSearch />}></Route>
+                <Route path="/signup" element={<SignUp />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
+                <Route path="/daily" element={<DailyExpenditure />}></Route>
             </Routes>
         </>
     );
