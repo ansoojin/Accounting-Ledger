6c2e0c8c91f257849e7852855b20faf625628864
kucodemaster <jh9485@korea.ac.kr>
2022-07-04 11:08:01 +0900

    DOCS : Add comments

--- server/event/event-model.js
+++ server/event/event-model.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const autoIdSetter = require('../services/auto-id-setter');
 
+/*
+각각의 수입과 지출 내역을 event라고 합니다.
+title : 제목
+content : 내용
+date : 날짜
+amount : 금액(지출이면 음수, 수입이면 양수)
+userId : event를 귀속시킬 유저의 아이디
+onBalance : True(유저 잔액에 반영), False(유저 잔액에 반영하지 않음)
+*/
 const eventSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
--- server/user/user-model.js
+++ server/user/user-model.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const autoIdSetter = require('../services/auto-id-setter');
 
+// 이메일 유효성 검증
 var validateEmail = function(email) {
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email)
 };
 
+/*
+유저 정보를 관리하는 테이블
+realName : 실명
+userId : 로그인에 사용되는 아이디
+userName : 유저가 설정한 닉네임
+password : 비밀번호
+balance : 잔액
+userMail : 유저의 이메일
+*/
 const userSchema = new Schema({
   realName: { 
     type: String,
